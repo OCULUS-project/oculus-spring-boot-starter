e6157348016724909582d972fe462d4ce65ea52e
jakubriegel <jakubriegel112@gmail.com>
2019-11-12 21:33:28 +0100

    0.2.2 - springfox fixed

--- src/main/kotlin/pl/poznan/put/oculus/boot/config/SwaggerConfig.kt
+++ src/main/kotlin/pl/poznan/put/oculus/boot/config/SwaggerConfig.kt
@@ -4,6 +4,14 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.hateoas.client.LinkDiscoverers
+import org.springframework.hateoas.mediatype.hal.HalLinkDiscoverer
+import org.springframework.hateoas.server.LinkRelationProvider
+import org.springframework.hateoas.server.core.DelegatingLinkRelationProvider
+import org.springframework.hateoas.server.core.EvoInflectorLinkRelationProvider
+import org.springframework.plugin.core.SimplePluginRegistry
+import org.springframework.plugin.core.support.PluginRegistryFactoryBean
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -11,7 +19,7 @@ import springfox.documentation.service.ApiInfo
 import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 /**
@@ -28,7 +36,7 @@ annotation class PublicAPI
  * To add controller to Swagger use [PublicAPI] annotation
  */
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2
 class SwaggerConfig (
     @Value("\${swagger.name}")
     val name: String,
@@ -43,7 +51,7 @@ class SwaggerConfig (
 ) {
     @Bean
     fun api(): Docket {
-        logger.debug("initializing Swagger bean")
+        logger.info("initializing Swagger bean")
         return Docket(DocumentationType.SWAGGER_2)
             .useDefaultResponseMessages(false)
             .select()
@@ -61,6 +69,27 @@ class SwaggerConfig (
             .licenseUrl("#")
             .version(version)
             .build()
+    }
+
+    // TODO: remove these beans after Springfox gets fixed for Spring HATEOAS
+    @Bean
+    fun discoverers(): LinkDiscoverers {
+        val plugins = listOf(HalLinkDiscoverer())
+        return LinkDiscoverers(SimplePluginRegistry.create(plugins))
+    }
+
+    @Bean
+    fun provider(): LinkRelationProvider {
+        return EvoInflectorLinkRelationProvider()
+    }
+
+    @Primary
+    @Bean
+    fun relProviderPluginRegistry2(): PluginRegistryFactoryBean<LinkRelationProvider, LinkRelationProvider.LookupContext> {
+        val factory = PluginRegistryFactoryBean<LinkRelationProvider, LinkRelationProvider.LookupContext>()
+        factory.setType(LinkRelationProvider::class.java)
+        factory.setExclusions(arrayOf(DelegatingLinkRelationProvider::class.java))
+        return factory
     }
 
     companion object {
